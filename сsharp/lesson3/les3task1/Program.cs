093e895161f5a99d8b6bef3eb6c9611f112cb787
GummyDonkey <kpebetkabtankax@gmail.com>
2022-10-26 01:33:55 +0500

    3_1FixIt

--- сsharp/lesson3/les3task1/Program.cs
+++ сsharp/lesson3/les3task1/Program.cs
@@ -1,4 +1,5 @@
-﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Не применять строки!
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно 
+//палиндромом. Не применять строки!
 //14212 -> нет
 //12821 -> да
 //23432 -> да
@@ -6,12 +7,12 @@
 
 int Prompt(string message)        //читаем число
 {
-    System.Console.Write("Введите пятизначное число:");
+    System.Console.Write(message);
     int number = Convert.ToInt32(Console.ReadLine());
     return number;
 }
 
-bool ValidateNumber(int number)               // проверка на диапазон чисел. не до конца понятно как рабоает
+bool ValidateNumber(int number)
 {
     if (number < 10000 && number > 99999)
     {
@@ -26,30 +27,48 @@ int Reverse(int number)
     int reverse = 0;
     while (number > 0)
     {
-        
-        int value = number % 10;    // посл цифра числа
-        reverse = reverse * 10 + value;     //???Локальная переменная или параметр с именем "reverse" нельзя объявить в 
-                                                //   данной области, так как это имя используется во включающей локальной области для 
-                                                //   определения локальной переменной или параметра
-                                                //???Использование локальной переменной "reverse", которой не присвоено значение
-        int tempnumber = number / 10;       
+        int value = number % 10;
+        reverse = reverse * 10 + value;
+        int tempnumber = number / 10;
         Console.Write($"1 -> {tempnumber} - {reverse}");
     }
-    return reverse;                             //Имя "reverse" не существует в текущем контексте.
+    return reverse;
 }
 
-int number = Prompt("Введите ваше число: ");   //присваиваем число
+bool IsAPalindrome(int number)
+{
+
+    if (number == Reverse(number))
+    {
+        Console.WriteLine(Reverse(number));
+        Console.WriteLine(number);
+        return number == Reverse(number);
+    }
+    else
+    {
+        Console.WriteLine(Reverse(number));
+        Console.WriteLine(number);
+        return false;
+        
+    }
+    
 
 
-if (ValidateNumber(number))    // копипаст (хз) но без нее не работает???
+    // почему вытягиваем Reverse(number) мы ведь возвращали reverse. 
+    //Или мы тут упоминаем метод как мы его обозвали выше? 
+    //т.к. вохвращает значение которое записывается в number этого метода?
+}
+
+
+
+int number = Prompt("Введите ваше число: ");
+
+
+if (ValidateNumber(number))
 {
     return;
 }
 
-bool IsAPalindrome(int number);
-{
-    return (number == Reverse(number));
-}
 if (IsAPalindrome(number))
 {
     Console.Write("Ваше число является палиндромом");
@@ -58,24 +77,4 @@ else
 {
     Console.Write("Ваше число НЕ является палиндромом");
 }
-
-/*
-int Oldn = N;
-int Newn = 0;
-if (!ValidateNumber(N))
-{
-    return;
-}
-while (N > 0)
-{
-    int Value = N % 10;
-    Newn = Newn * 10 + Value;
-    N = N / 10;
-}
-if (Oldn == Newn)
-{
-    System.Console.WriteLine("asdasd");
-}
-*/
-
 
