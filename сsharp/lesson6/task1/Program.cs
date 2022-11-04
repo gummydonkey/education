9ba1743055f6602cbbd362953841bd62c884be0d
Pavel <kpebetkabtankax@gmail.com>
2022-11-04 19:42:51 +0500

    l6inProgress

--- сsharp/lesson6/task1/Program.cs
+++ сsharp/lesson6/task1/Program.cs
@@ -2,3 +2,43 @@
 //Разделить ввод данных, от расчета данных (Одна функция вводит данные, другая функция подсчитывает количество положительных)
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
+
+Console.WriteLine();
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int value = Prompt("Сколько чисел вы планируете ввести?: ");
+System.Console.WriteLine();
+
+int[] CreateArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"Введите {i+1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int CountMeter(int[] array)
+{  
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count++;
+    }
+    System.Console.WriteLine();
+    return count;
+}
+
+
+Console.WriteLine($"Подсчёт чисел больше 0");
+int final_value = Convert.ToInt32(CountMeter(CreateArray(value)));
+System.Console.WriteLine($"Колличество чисел больше 0 => {final_value}");
+Console.WriteLine();
--- сsharp/lesson6/task3/Program.cs
+++ сsharp/lesson6/task3/Program.cs
@@ -4,3 +4,50 @@
 // 1 2 3
 // 3 4 5
 // 3+5=8, 1+2+3=6, 8-6=2
+
+int[,] array2D = new int[,] {{ 1, 2, 3 }, { 3, 4, 5 }};
+
+
+int [,] Print(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            Console.Write($"{array2D[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return array2D;
+}
+
+int maxFirstLine(int[,] array2D)
+{
+    int maxFirst = array2D[0,0];
+    for (int j = 0; j < array2D.GetLength(1); j++)
+    {
+        if (maxFirst < array2D[0,j])
+            maxFirst = array2D[0,j];
+    }
+    Console.WriteLine($"{maxFirst} ");
+    return maxFirst;
+}
+
+
+int maxSecondLine(int[,] array2D)
+{
+    int maxSecond = array2D[1,0];
+    for (int j = 0; j < array2D.GetLength(1); j++)
+    {
+        if (maxSecond < array2D[1,j])
+            maxSecond = array2D[1,j];
+    }
+    Console.WriteLine($"{maxSecond} ");
+    return maxSecond;
+}
+
+
+Print(array2D);
+maxFirstLine(array2D);
+maxSecondLine(array2D);
