2095014daece290fb09f8e0771caccfce88610e7
gummydonkey <101453486+gummydonkey@users.noreply.github.com>
2022-11-22 00:40:19 +0500

    l9t2finish

--- сsharp/lesson9/task2/Program.cs
+++ сsharp/lesson9/task2/Program.cs
@@ -10,11 +10,10 @@ int Prompt(string message)
     int number = Convert.ToInt32(Console.ReadLine());
     return number;
 }
-
 int PrintSum(int m, int n)
 {
     if (n < m) return 0;
-    else return n + PrintSum(m ,(n - 1));
+    else return n + PrintSum(m, (n - 1));
 }
 
 
